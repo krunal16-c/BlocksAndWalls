9770c95c1e0d16cd3bc54978e947dc70a0eda16f
Owner <bill.colak@gmail.com>
2023-01-06 22:53:10 -0600

    Added PlayerSCript.cs and line of comment on line 10.

---
+++ My Project/Assets/PlayerSCript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSCript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     // This is a new line of comment.   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
